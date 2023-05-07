df62e2379021f04a69d9f30b1d00ed948310af71
Lily <lily.maksutova@mail.ru>
2023-05-07 20:22:32 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { useState } from "react";
+
+function App() {
+  const [outputText, setOutputText] = useState("Initial text");
+
+  function updateTextHandler() {
+    setOutputText("Text was changed");
+  }
+
+  return (
+    <>
+      <button onClick={updateTextHandler}>Click to change text</button>
+      <p>{outputText}</p>
+    </>
+  );
+}
+
+// function App() {
+//   const [isSubmitted, setIsSubmitted] = useState(false);
+
+//   function submitHandler() {
+//     setIsSubmitted(true);
+//   }
+//   return (
+//       <button onClick={submitHandler}>
+//         {isSubmitted ? "Loading…" : "Submit"}
+//       </button>
+//   );
+// }
+
+// function Greeting() {
+//   return <p>Welcome to this book!</p>;
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <h2>Hello World!</h2>
+//       <Greeting />
+//     </div>
+//   );
+// }
+
+export default App;
