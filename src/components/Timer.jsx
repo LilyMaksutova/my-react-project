5c39e2e7d8dca5b13f4044aa75ded7692fb03d99
Lily <lily.maksutova@mail.ru>
2023-09-03 23:15:21 +0300

    add condition to reset

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,50 +1,22 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Space } from 'antd';
 
-function Timer() {
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(1);
-  const [seconds, setSeconds] = useState(5);
-  let timer = useRef();
+function Timer({ restTimeInSecs = 10 }) {
+  const [seconds, setSeconds] = useState(0);
+  const deadline = Date.now() + restTimeInSecs * 1000;
 
   const start = () => {
-    timer = setInterval(() => {
-      setSeconds(seconds - 1);
+    const interval = setInterval(() => {
+      const timeDiffInSecs = (deadline - Date.now()) / 1000;
+      setSeconds(Math.round(timeDiffInSecs));
 
-      if (seconds === 0) {
-        setMinutes(minutes - 1);
-        setSeconds(59);
-      }
-
-      if (minutes === 0 && seconds === 0) {
-        setHours(hours - 1);
-        setMinutes(59);
-      }
-
-      if (hours === 0 && minutes === 0 && seconds === 0) {
-        setHours(0);
-        setMinutes(0);
-        setSeconds(0);
+      if (timeDiffInSecs < 0) {
+        clearInterval(interval);
       }
     }, 1000);
   };
 
-  useEffect(() => {
-    start();
-    return () => clearInterval(timer);
-  });
-
-  const reset = () => {
-    setSeconds(0);
-    setMinutes(0);
-    setHours(0);
-  };
-
-  const stop = () => {
-    clearInterval(timer);
-  };
-
-  const restart = () => {};
+  useEffect(start, []);
 
   return (
     <div
@@ -57,11 +29,7 @@ function Timer() {
         height: '100%',
       }}
     >
-      <h1>
-        {hours < 10 ? `0${hours}` : hours}:
-        {minutes < 10 ? `0${minutes}` : minutes}:
-        {seconds < 10 ? `0${seconds}` : seconds}
-      </h1>
+      <h1>{`${Math.floor(seconds / 60)} : ${seconds % 60} `}</h1>
 
       <Space wrap>
         <Button
@@ -73,18 +41,16 @@ function Timer() {
         </Button>
         <Button
           type="primary"
-          onClick={reset}
+          /* onClick={reset} */
           style={{ backgroundColor: 'red' }}
         >
           Сброс
         </Button>
-        <Button type="primary" onClick={stop}>
-          Стоп
-        </Button>
+        <Button type="primary" /* onClick={stop} */>Стоп</Button>
         <Button
           type="primary"
           style={{ backgroundColor: 'orange' }}
-          onClick={restart}
+          /* onClick={restart} */
         >
           Перезапуск
         </Button>
